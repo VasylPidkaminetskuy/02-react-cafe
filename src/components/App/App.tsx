4ff917f930b24268c45c499634840576805f58ad
Vasyl <pidkaminetskuy@gmail.com>
2025-09-23 01:08:08 +0300

    push task

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import CafeInfo from '../Cafeinfo/Cafeinfo';
+import css from './App.module.css';
+import type {Votes, VoteType} from '../../types/votes';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+  const handleVote = (type: VoteType) => {
+    setVotes(votes => ({ ...votes, [type]: votes[type] + 1 }));
+  };
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const canreset = () => {
+    return votes.good > 0 || votes.neutral > 0 || votes.bad > 0;
+  }
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canreset={canreset} />
+      <VoteStats votes={votes}  totalVotes={0} positiveRate={0}/>
+    </div> 
+  );
+}
---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,28 @@
+import type { VoteType } from '../../types/votes';
+import css from './VoteOptions.module.css';
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+    onReset: () => void;
+    canreset: () => boolean;
+
+}
+
+export default function VoteOptions({ onVote, onReset, canreset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+          <button onClick={() => {onVote('good')}} className={css.button}>
+        Good
+      </button>
+      <button onClick={() => {onVote('neutral')}} className={css.button}>
+        Neutral
+      </button>
+      <button onClick={() => {onVote('bad')}} className={css.button}>
+        Bad
+      </button>
+      <button onClick={onReset} className={`${css.button} ${css.reset}`}>
+        Reset
+      </button>
+    </div>
+  );
+}
