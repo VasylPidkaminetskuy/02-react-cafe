3dadf1384c37ddf0a4a63e6af67baebd2d882c0b
Vasyl <pidkaminetskuy@gmail.com>
2025-09-23 01:28:40 +0300

    push task

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import CafeInfo from '../Cafeinfo/Cafeinfo';
 import css from './App.module.css';
-import type {Votes, VoteType} from '../../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
@@ -14,15 +15,32 @@ export default function App() {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   const canreset = () => {
     return votes.good > 0 || votes.neutral > 0 || votes.bad > 0;
-  }
+  };
 
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canreset={canreset} />
-      <VoteStats votes={votes}  totalVotes={0} positiveRate={0}/>
-    </div> 
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canreset={canreset}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />  
+      ) : (
+        <Notification/>
+      )}
+    </div>
   );
 }
