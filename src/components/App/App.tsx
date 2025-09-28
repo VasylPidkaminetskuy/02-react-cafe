830700cd0c034f07990022ad1ed87c8f61954ee6
Vasyl <pidkaminetskuy@gmail.com>
2025-09-28 23:06:27 +0300

    push new

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from '../CafeInfo-template/CafeInfo';
+import CafeInfo from '../CafeInfo/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
